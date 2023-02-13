7b1ddb49f5b0301e3990db1886e3882808cd4efe
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-14 00:22:30 +0530

    Add : Added Player Having Option

--- PLayerPosition.cs
+++ PLayerPosition.cs
@@ -10,9 +10,43 @@ namespace SnakeLadderProblem
     {
         public static void player()
         {
-            Random random= new Random();
-            int player = random.Next(1, 7);
-            Console.WriteLine("Player Roll the Dice : "+player);
+            Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+            int position = 0;
+            int count = 0;
+            int lastPosition = 100;
+            Random random = new Random();
+            Random random1 = new Random();
+            for (int i = 0; i < lastPosition; i++)
+            {
+                while (count <= lastPosition)
+                {
+                    int result = random.Next(0, 3);
+                    int Dice = random1.Next(1, 7);
+                    if (position == 0)
+                    {
+                        if (result == 0)
+                        {
+                            Console.WriteLine("Snake");
+                            Console.WriteLine(" Player moves behind by the number of position received in the dice");
+                            position = position - Dice;
+                            Console.WriteLine("position moves back : " + position);
+                        }
+                        else if(result == 1) 
+                        {
+                            Console.WriteLine("Ladder");
+                            Console.WriteLine(" Player moves ahead by the number of position received in the die");
+                            position = position + Dice;
+                            Console.WriteLine("position moves ahead : " + position);
+                        }
+                        else
+                        {
+                            Console.WriteLine("No Play");
+                            Console.WriteLine("Player Will Stay in the same position");
+                        }
+                    }
+                }
+                count++;
+            }
         }
     }
 }
